e63438074c023e4c0a0ac113e024d598828a210e
Mateusz <mateusz@rustle.pl>
2025-04-16 13:47:43 +0200

    After migration from 1.13 to 1.14, no errors

--- tests/Application/config/bundles.php
+++ tests/Application/config/bundles.php
@@ -67,4 +67,5 @@ return [
     Sylius\Calendar\SyliusCalendarBundle::class => ['all' => true],
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Sylius\Abstraction\StateMachine\SyliusStateMachineAbstractionBundle::class => ['all' => true],
 ];
